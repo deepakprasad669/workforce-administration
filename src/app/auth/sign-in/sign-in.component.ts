7a36ea8928424eb11e9aa011b9e9f8517844b313
Deepak_Prasad <deepak.prasad920@gmail.com>
2025-01-31 15:56:22 +0530

    sign in component work

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,11 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  signInForm!: FormGroup;
+  submitted: boolean = false;
+  emailPattern = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$';
 
-  constructor(){}
+  constructor(
+    private fb: FormBuilder
+  ){}
 
     ngOnInit(): void {
-      console.log("signIn run")
+      this.signInForm = this.fb.group({
+        email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      })
+    }
+
+    signInFormSubmit() {
+      this.submitted = true;
+      if (this.signInForm.valid) {
+
+      }
     }
 
 }
